1bbf44cab282f761c00b05ce26ef66fc4bd4cf36
killinux <killinux@gmail.com>
2026-04-18 08:49:14 +0000

    Eyelid: +Y 1mm (follow eyeball curvature), Z upper -9 lower +8

--- experimental/morph_transfer_poc.py
+++ experimental/morph_transfer_poc.py
@@ -664,18 +664,18 @@ INASE_RECIPES = {
     },
     # --- Eyelids ---
     'まばたき': {  # both eyes closed
-        EYELID_UPPER_L: (0, 0, -8),
-        EYELID_UPPER_R: (0, 0, -8),
-        EYELID_LOWER_L: (0, 0, +7),
-        EYELID_LOWER_R: (0, 0, +7),
+        EYELID_UPPER_L: (0, +1, -9),
+        EYELID_UPPER_R: (0, +1, -9),
+        EYELID_LOWER_L: (0, +1, +8),
+        EYELID_LOWER_R: (0, +1, +8),
     },
     'ウィンク': {  # model-left eye wink (viewer's right)
-        EYELID_UPPER_L: (0, 0, -8),
-        EYELID_LOWER_L: (0, 0, +7),
+        EYELID_UPPER_L: (0, +1, -9),
+        EYELID_LOWER_L: (0, +1, +8),
     },
     'ウィンク右': {  # model-right eye wink (viewer's left)
-        EYELID_UPPER_R: (0, 0, -8),
-        EYELID_LOWER_R: (0, 0, +7),
+        EYELID_UPPER_R: (0, +1, -9),
+        EYELID_LOWER_R: (0, +1, +8),
     },
 }
 
